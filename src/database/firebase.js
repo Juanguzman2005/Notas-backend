9c6e354230ac2f0e3a687c9c7c1fcd872678e150
Juanguzman2005 <juanca.guzman1786@gmail.com>
2025-12-12 09:47:21 -0500

    Use FIREBASE_SERVICE_ACCOUNT env var for Firebase Admin

--- src/database/firebase.js
+++ src/database/firebase.js
@@ -5,16 +5,22 @@ const YAML = require("yamljs");
 // Cargar configuración YAML (para demostrar uso de YAML)
 const config = YAML.load(path.join(__dirname, "..", "..", "config.yml"));
 
-const serviceAccount = require("../../firebase-key.json");
+const admin = require("firebase-admin");
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
-});
+if (!admin.apps.length) {
+  const serviceAccountStr = process.env.FIREBASE_SERVICE_ACCOUNT;
+
+  if (!serviceAccountStr) {
+    throw new Error("FIREBASE_SERVICE_ACCOUNT no está configurada en variables de entorno");
+  }
+
+  const serviceAccount = JSON.parse(serviceAccountStr);
+
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  });
+}
 
 const db = admin.firestore();
 
-module.exports = {
-  admin,
-  db,
-  config
-};
+module.exports = { admin, db };
